d0c2532453a558a8ce7e3b1b84387d7296ee195a
Lucia Gil <luciagil5952@gmail.com>
2022-06-13 16:43:32 -0400

    Entered cities get added to local storage, added cities button functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-var button = $('#submit-btn');
+var checkWeatherBtn = $('#submit-btn');
 var inputVal = $('#user-input');
-var citiesBtn = $(".citiesBtn");
+var citiesBtn = $("#citiesBtn");
 var currentWeather = $('#display-current-weather');
 var cityName = $(`#city-name`);
 var forecastDiv = $("#forecastDiv");
@@ -12,39 +12,59 @@ var cities = [{
     lon: -84.4146,
 }];
 
+
+
+// Displays current date
 $("#current-date").text(today);
+
 // Runs an API that with gather the input value and get its latitude and longitude, then executes those values to the weather app function
 getLatLon = () => {
     fetch(`http://api.positionstack.com/v1/forward?access_key=e787a7efe77d71459dd6e00b2fe0e586&query=${inputVal.val()}&country=US`)
     .then(response => response.json())
     .then(data => {
+        
+        
+
+        // Where we store new city information
         var cityInput = {
             name: inputVal.val(),
             lat: data.data[0].latitude,
             lon: data.data[0].longitude,
         }
-        cities.push(cityInput);
+
+
+        // push city input object to cities array
+       
+            cities.push(cityInput);
+            storeCities();
+    
+        
         // Runs the function cityBtn which creates a button for every new city entered
         cityBtn();
+        // Runs weather data for the lat and lon of city entered
         getCurrentWeather(cityInput.lat,cityInput.lon);
-        console.log(cities);
+       
+        
     })
 }
 
-// On click run get latitude/longitude function and change header
-button.click(e => {
+// SAVE CITIES
+storeCities = () => {
+    localStorage.setItem("savedCities", JSON.stringify(cities));
+}
+// When check weather button is clicked then getLatLon function and change header
+checkWeatherBtn.click(e => {
     getLatLon();    
 });
 
 // Gets current wather from API, appends info adds it to local storage
 getCurrentWeather = (lat, lon) => {
-    var todaysWeatherAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,minutely&appid=eca4a1901a7a41c8ceead4f3f120e01b`;
-    fetch (todaysWeatherAPI)
+    var weatherAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,minutely&appid=eca4a1901a7a41c8ceead4f3f120e01b`;
+    fetch (weatherAPI)
     .then(response => response.json())
     .then(data => {
         console.log(data);
        
-        
         // Display name, date, temp,wind speed, humidity, uv index
         var temp = data.current.temp;
         var windSpeed = data.current.wind_speed;
@@ -53,6 +73,8 @@ getCurrentWeather = (lat, lon) => {
         // Creates currentWeather div with information on the current weather
         currentWeather.empty();
         cityName.empty();
+
+        // RUN CITY NAME APPEND H4 WHEN CLICKED AND WHEN WRITTEN AND ENTERED
         cityName.append(`<h4 class = cap>${inputVal.val()}</h4>`);
         currentWeather.append(`<ul> <li> Temp: ${temp} F </li> <li>Wind speed: ${windSpeed} MPH </li> <li>Humidity: ${humidity}% </li> <li>UV Index: ${uvi}</li></ul>`);
     
@@ -61,16 +83,12 @@ getCurrentWeather = (lat, lon) => {
         for (let i=0; i < 5; i++) {
             var date = new Date(data.daily[i].dt * 1000);
             var dateFormat = moment(date).format("dddd, MMMM Do YYYY");
-            console.log(dateFormat);
             var temp = data.daily[i].temp.day;
             var humidity = data.daily[i].humidity;
             var forecastedWindSpeed = data.daily[i].wind_speed;
-            console.log(temp + `F`);
             var ForecastCol = $(`<div class="col card card-body forecastCards col-sm-2" data-type="` + i + `"> <ul><li>${dateFormat}</li><li>Temp: ${temp} F° </li> <li>Humidity: ${humidity}% </li><li> Wind Speed: ${forecastedWindSpeed} MPH <li></ul></div>`);
             forecastDiv.append(ForecastCol);
         }
-
-        localStorage.setItem("city",)
         
     })
     .catch(error => console.log('error'))
@@ -82,12 +100,27 @@ cityBtn = () => {
     citiesBtn.empty();
     cities.forEach(city => {
         citiesBtn.append(`<button class = "btn btn-primary citiesBtn cap" value="${city}">${city.name}</button><br/>`);
+    
     })
 
 }
 
-citiesBtn.on("click", getCurrentWeather(citiesBtn.val().lat,citiesBtn.val().lon));
+// When you click a city button then that cities weather info will be displayed
+citiesBtn.on("click", function (e) {
+    var cityText = e.target.textContent
+    console.log(cityText);
+    var matchingCity = getItemByName(cities, cityText);
+    getCurrentWeather(matchingCity.lat,matchingCity.lon);
+    cityName.empty();
+    cityName.append(`<h4 class = cap>${cityText}</h4>`);
 
+});
 
-
+// Gets object if button text equals name value in city object
+getItemByName = (cities, name) => {
+var ret = cities.filter(function (city) {
+    return city.name === name;
+});
+return ret[0];
+}
 
